2b1d014ece5b5ac869c754094436655f695fd8da
DDE <DDE@59.23.163.206>
2020-07-22 22:35:38 +0900

    20200722spring

---
+++ src/main/java/com/springbook/board/BoardController.java
@@ -0,0 +1,36 @@
+package com.springbook.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+	
+	@Autowired
+	private BoardService service;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String boardList(Model model) {
+		
+		model.addAttribute("data", service.getBoardList());	
+		return "board/list";
+	}
+	
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public String boardWrite() {
+		return "board/write";
+	}
+	
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String boardWrite(BoardVO param) {
+		
+		System.out.println("title : " + param.getTitle());
+		System.out.println("ctnt : " + param.getCtnt());
+		return "";
+	}
+	
+}
---
+++ src/main/java/com/springbook/board/BoardService.java
@@ -0,0 +1,18 @@
+package com.springbook.board;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardMapper mapper;
+	
+	public List<BoardVO> getBoardList() {
+		return mapper.getBoardList();
+	}
+
+}
